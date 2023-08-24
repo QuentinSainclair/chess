29f1623c74503cff926f86fc05cd00f2f39c89ef
Quentin <quentin.sainclair@kwfrance.com>
2023-08-24 21:42:45 +0200

    fonction toggle OK

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-//Fonction du choix de la couleur
 function colorChoice() {
     let colorWhite = true;
     const toggle = document.getElementsByClassName('toggle')[0];
     let carre = document.getElementsByClassName('carre')[0];
     let carre2 = document.getElementsByClassName('carre2')[0];
+    let isProcessingClick = false;
 
     toggle.addEventListener("click", function() {
+        if (isProcessingClick) {
+            return;
+        }
+        isProcessingClick = true;
         if (colorWhite === true) {
             colorWhite = false;
             carre.classList.add("masquerEchiquier");
-            carre2.classList.remove("carre2")
-        } else {
+            carre2.classList.remove("carre2");
+        }  else {
             colorWhite = true;
             carre.classList.remove("masquerEchiquier");
-            carre2.classList.add("carre2")
+            carre2.classList.add("carre2");
         }
-        console.log(colorWhite)
+        setTimeout(function() {
+            isProcessingClick = false;
+        }, 100);
     });
 }
 
