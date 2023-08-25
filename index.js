2c3b27956715caedc728cadf034411a312e2c802
Quentin <quentin.sainclair@kwfrance.com>
2023-08-25 13:08:39 +0200

    déplacement des pièces

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let tour = "blancs";
+
+//Fonction pour inverser l'échiquier
 function colorChoice() {
     let colorWhite = true;
     const toggle = document.getElementsByClassName('toggle')[0];
@@ -24,5 +27,56 @@ function colorChoice() {
         }, 100);
     });
 }
-
 colorChoice();
+
+
+// Fonctions de déplacement des pions
+const pieces = document.querySelectorAll('.piece');
+let pieceBeingDragged = null;
+
+pieces.forEach(piece => {
+    piece.addEventListener('dragstart', dragStart);
+    piece.addEventListener('dragend', dragEnd);
+});
+
+function dragStart(event) {
+    pieceBeingDragged = event.target;
+    event.dataTransfer.setData('text/plain', '');
+}
+
+function dragEnd() {
+    pieceBeingDragged = null;
+}
+
+const squares = document.querySelectorAll('.case');
+
+squares.forEach(square => {
+    square.addEventListener('dragover', dragOver);
+    square.addEventListener('dragenter', dragEnter);
+    square.addEventListener('dragleave', dragLeave);
+    square.addEventListener('drop', dragDrop);
+});
+
+function dragOver(event) {
+    event.preventDefault();
+}
+
+function dragEnter(event) {
+    event.preventDefault();
+    this.classList.add('hovered'); // Ajoute une classe pour indiquer la zone de dépôt
+}
+
+function dragLeave() {
+    this.classList.remove('hovered'); // Supprime la classe lorsque la pièce quitte la zone
+}
+
+function dragDrop() {
+    this.appendChild(pieceBeingDragged); // Déplace la pièce dans la nouvelle square
+    this.classList.remove('hovered'); // Supprime la classe hover
+}
+
+//Fonction restart
+const reloadButton = document.getElementById('restart');
+reloadButton.addEventListener('click', function() {
+    location.reload();
+});
